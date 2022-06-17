d2099e156df8d4ea56396679b5ff23cb97c0367f
cyai <seriesscar@gmail.com>
2022-06-17 15:48:57 +0530

    Added Pedestrain detection funtionality

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import cv2
 
 cars_cascade = cv2.CascadeClassifier('haarcascade_car.xml')
+body_cascade = cv2.CascadeClassifier('fullbody.xml')
 
-def detect_cars(frame):
+
+def detect_cars_and_pedestrain(frame):
     cars = cars_cascade.detectMultiScale(frame, 1.15, 4)
+    pedistrain = body_casccade.detectMultiSclae(frame, 1.15, 4)
     for (x, y, w, h) in cars:
-        cv2.rectangle(frame, (x, y), (x+w,y+h), color=(0, 255, 0), thickness=2)
+        cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(255, 0, 0), thickness=2)
+        cv2.rectangle(frame, (x, y), (x+w, y_h), color=(0, 255, 0), thickness=2)
+    
+    for(x, y, w, h) in pedistrain:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
+
     return frame
 
 def Simulator():
