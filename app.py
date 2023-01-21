4350893fc75956f691ea707ca554e074bbe4392b
jovyinny <jovinemutelani@gmail.com>
2023-01-21 12:05:54 +0300

    fix: fixed minor errors

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ body_cascade = cv2.CascadeClassifier('fullbody.xml')
 
 def detect_cars_and_pedestrain(frame):
     cars = cars_cascade.detectMultiScale(frame, 1.15, 4)
-    pedistrain = body_casccade.detectMultiSclae(frame, 1.15, 4)
+    pedistrain = body_cascade.detectMultiScale(frame, 1.15, 4)
     for (x, y, w, h) in cars:
         cv2.rectangle(frame, (x+1, y+1), (x+w,y+h), color=(255, 0, 0), thickness=2)
-        cv2.rectangle(frame, (x, y), (x+w, y_h), color=(0, 255, 0), thickness=2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 0), thickness=2)
     
     for(x, y, w, h) in pedistrain:
         cv2.rectangle(frame, (x, y), (x+w, y+h), color=(0, 255, 255), thickness=2) 
@@ -22,7 +22,7 @@ def Simulator():
         ret, frame = CarVideo.read()
         controlkey = cv2.waitKey(1)
         if ret:        
-            cars_frame = detect_cars(frame)
+            cars_frame = detect_cars_and_pedestrain(frame)
             cv2.imshow('frame', cars_frame)
         else:
             break
